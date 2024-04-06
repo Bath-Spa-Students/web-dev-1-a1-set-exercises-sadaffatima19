31878459e979b98ebb0b483a4a98dc87fc485ed6
Sadaf Fatima <fatimasadaf68@gmail.com>
2024-04-07 01:35:28 +0400

    index.js

--- Chapter-5-Embedded Content/Exercises/Bonus/index.js
+++ Chapter-5-Embedded Content/Exercises/Bonus/index.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const soundboard = document.getElementById('soundboard');
-    const audioFiles = ["C:\Users\marwa\Documents\GitHub\web-dev-1-a1-set-exercises-sadaffatima19\Chapter-5-Embedded Content\Exercises\Audio Sampler\Audio\ah-ha.mp3",
-    "C:\Users\marwa\Documents\GitHub\web-dev-1-a1-set-exercises-sadaffatima19\Chapter-5-Embedded Content\Exercises\Audio Sampler\Audio\back-of-the-net.mp3",
-"C:\Users\marwa\Documents\GitHub\web-dev-1-a1-set-exercises-sadaffatima19\Chapter-5-Embedded Content\Exercises\Audio Sampler\Audio\bangoutoforder.mp3",
-"C:\Users\marwa\Documents\GitHub\web-dev-1-a1-set-exercises-sadaffatima19\Chapter-5-Embedded Content\Exercises\Audio Sampler\Audio\dan.mp3",
-"C:\Users\marwa\Documents\GitHub\web-dev-1-a1-set-exercises-sadaffatima19\Chapter-5-Embedded Content\Exercises\Audio Sampler\Audio\emailoftheevening.mp3",
-"C:\Users\marwa\Documents\GitHub\web-dev-1-a1-set-exercises-sadaffatima19\Chapter-5-Embedded Content\Exercises\Audio Sampler\Audio\hellopartridge.mp3",
-"C:\Users\marwa\Documents\GitHub\web-dev-1-a1-set-exercises-sadaffatima19\Chapter-5-Embedded Content\Exercises\Audio Sampler\Audio\iateascotchegg.mp3",
-"C:\Users\marwa\Documents\GitHub\web-dev-1-a1-set-exercises-sadaffatima19\Chapter-5-Embedded Content\Exercises\Audio Sampler\Audio\imconfused.mp3"];
+    const audioFiles = [
+        "Audio Sampler\\ah-ha.mp3",
+        "Audio Sampler\\back-of-the-net.mp3",
+        "Audio Sampler\\bangoutoforder.mp3",
+        "Audio Sampler\\dan.mp3",
+        "Audio Sampler\\emailoftheevening.mp3",
+        "Audio Sampler\\hellopartridge.mp3",
+        "Audio Sampler\\iateascotchegg.mp3",
+        "Audio Sampler\\imconfused.mp3"
+    ];
 
     // Create audio elements for each audio file
     audioFiles.forEach(audioFile => {
         const audioElement = document.createElement('audio');
-        audioElement.src = `Audio Sampler/${audioFile}`;
+        audioElement.src = audioFile;
         audioElement.preload = 'auto';
 
         const button = document.createElement('button');
         button.className = 'audio-button';
-        button.textContent = audioFile.replace('.mp3', '');
+        button.textContent = audioFile.replace('Audio Sampler\\', '').replace('.mp3', '');
         button.addEventListener('click', () => {
             audioElement.currentTime = 0;
             audioElement.play();
